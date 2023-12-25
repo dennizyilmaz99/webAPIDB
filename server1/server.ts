c9f5d40b8cd76417b95c6ef695e35bcfa5b74a8b
Denniz Yilmaz <112471865+dennizyilmaz99@users.noreply.github.com>
2023-12-25 13:53:57 +0100

    ändrade från .js till .ts vet inte hur det hände

--- server1/server.ts
+++ server1/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import User from "./models/User.js";
+import User from "./models/User";
 
 mongoose
   .connect(
